d0ab707dcb0df092fb8deebe8205a41239f81740
McKrus <maciejkruszyniak@gmail.com>
2021-03-16 21:58:15 +0100

    make it better

---
+++ client/build/precache-manifest.ce873a88779a237525f3b2dfcf8e00c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ee20a0564e3c767025d238c3328688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca91f63960e94335cf4c",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "af2a1256e76f9033b24a",
+    "url": "/static/js/2.3ec2b8bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3ec2b8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca91f63960e94335cf4c",
+    "url": "/static/js/main.7dcbaa1d.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
